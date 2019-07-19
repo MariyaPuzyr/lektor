d02e89fbf829d177899a1cb0675ffc8d217d63f9
MariyaPuzyr <mariyapyzur@gmail.com>
2019-07-19 13:30:53 +0300

    change countdown in slider

--- src/js/main.js
+++ src/js/main.js
@@ -10,49 +10,10 @@ $(document).ready(function () {
 
 
   /* countdown */
-  // let currentTime = new Date(Date.now());
-  // let goalTime = new Date(document.querySelector('#counter-1').getAttribute("datetime"));
-  //
-  // let countdownTime = goalTime.getTime() - currentTime.getTime();
-  // const countdown = document.querySelector('.countdown');
-  // const countdownDays = countdown.querySelector('.days');
-  // const countdownHours = countdown.querySelector('.hours');
-  // const countdownMinutes = countdown.querySelector('.minutes');
-  // const countdownSeconds = countdown.querySelector('.seconds');
-  //
-  // function formatTimeString(timeNumber) {
-  //   let tempArray = timeNumber.toString().split('');
-  //
-  //   if (tempArray.length < 2) {
-  //     tempArray.unshift("0");
-  //   }
-  //
-  //   return tempArray.map((item) => {
-  //     return `<span>${item}</span>`;
-  //   }).join('');
-  // }
-  //
-  // function tick() {
-  //   countdownTime -= 1000;
-  //   let newDay = formatTimeString(Math.floor(countdownTime / 86400000));
-  //   let newHour = formatTimeString(Math.floor((countdownTime % 86400000) / 3600000));
-  //   let newMinute = formatTimeString(Math.floor(((countdownTime % 86400000) % 3600000) / 60000));
-  //   let newSecond = formatTimeString(Math.floor((((countdownTime % 86400000) % 3600000) % 60000) / 1000));
-  //
-  //   requestAnimationFrame(() => {
-  //     countdownDays.innerHTML = newDay;
-  //     countdownHours.innerHTML = newHour;
-  //     countdownMinutes.innerHTML = newMinute;
-  //     countdownSeconds.innerHTML = newSecond;
-  //   });
-  // };
-  //
-  // setInterval(tick, 1000);
-
   $('[data-countdown]').each(function() {
     var $this = $(this), finalDate = $(this).data('countdown');
     $this.countdown(finalDate, function(event) {
-      $this.html(event.strftime('%D days %H:%M:%S'));
+      $this.html(event.strftime("<span class='countdown-item'>"+ '%D'+"<span class='countdown-title'>дней</span></span>" + "<span class='countdown-item'>"+ '%H'+"<span class='countdown-title'>часов</span></span>" + "<span class='countdown-item'>"+ '%M'+"<span class='countdown-title'>минут</span></span>" + "<span class='countdown-item'>"+ '%S'+"<span class='countdown-title'>секунд</span></span>"));
     });
   });
 });
